a7e1d3258f63edbb3355eb7fd7ef534234bdd0d1
Clemence <geeklego@gmail.com>
2023-07-08 17:37:09 +0200

    dialogue conditions

--- Assets/Scripts/Dialogue/DialogueBranch.cs
+++ Assets/Scripts/Dialogue/DialogueBranch.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "Dialogue/DialogueBranch", fileName = "DialogueBranch")]
 public class DialogueBranch : ScriptableObject
 {
+    public DialogueCondition condition;
+    public int SusModifier;
+    
+    [Space(10)]
+
     public List<TextEntry> entries = new();
     public List<DialogueBranch> following = new();
 }
