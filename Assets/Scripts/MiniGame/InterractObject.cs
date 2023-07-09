f129dc278ee69c50d49159c8379cc76d612c1265
Promethee Toneatti <promethee.toneatti@telecom-sudparis.eu>
2023-07-09 14:28:16 +0200

    cadre scene

--- Assets/Scripts/MiniGame/InterractObject.cs
+++ Assets/Scripts/MiniGame/InterractObject.cs
@@ -1,12 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics.Tracing;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
 public class InterractObject : MonoBehaviour
 {
+
+    [SerializeField] int numberOfClick = 1;
+    int currentClick = 0;
+    [SerializeField] Sprite newSprite;
     private void OnMouseDown()
     {
-        FindObjectOfType<MiniGame>().Win();
+        currentClick++;
+        if (currentClick == numberOfClick)
+        {
+            gameObject.GetComponent<SpriteRenderer>().sprite = newSprite;
+            FindObjectOfType<MiniGame>().Win();
+        }
+        else
+        {
+            gameObject.transform.position += new Vector3(1, 1,0);
+        }
+        
     }
 }
